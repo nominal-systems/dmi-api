83ce8d6f3ed4b8c1fba6fd560a6a84d647af8154
Miguel Araujo <miguel@linehq.com>
2021-03-04 12:14:32 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  root: true,
+  extends: ['plugin:@typescript-eslint/recommended', 'standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', '@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
