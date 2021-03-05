87c020abf2b6d33de4f872863d39665dbef54dcc
Miguel Araujo <miguel@linehq.com>
2021-03-05 17:02:33 -0400

    Add docker setup, set listen port from environment variable, set listen all address, edit README and add .env.example

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import {
   FastifyAdapter,
@@ -10,6 +12,11 @@ async function bootstrap () {
     AppModule,
     new FastifyAdapter(),
   )
-  await app.listen(3000)
+  const configService = app.get(ConfigService)
+  const PORT = configService.get('PORT')
+
+  await app.listen(PORT, '0.0.0.0')
+
+  Logger.log(`App listening on port ${PORT}`)
 }
 bootstrap()
