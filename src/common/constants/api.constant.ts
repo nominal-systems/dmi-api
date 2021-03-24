60608ce7fe97f9292b1dc0a0fb2121359a89a211
Miguel Araujo <miguel@linehq.com>
2021-03-24 10:50:53 -0400

    Add back uuid package and revert message structure

--- src/common/constants/api.constant.ts
+++ src/common/constants/api.constant.ts
@@ -1,1 +1,2 @@
 export const API_VERSION = 'v0'
+export const IE_MESSAGE_VERSION = '0.0.1'
--- src/common/utils/ieMessageBuilder.ts
+++ src/common/utils/ieMessageBuilder.ts
@@ -1,3 +1,6 @@
+import { v4 as uuidv4 } from 'uuid'
+import { IE_MESSAGE_VERSION } from '../constants/api.constant'
+
 interface IntergrationEngineOutgoingMessageData {
   providerConfiguration?: any
   integrationOptions?: any
@@ -5,8 +8,9 @@ interface IntergrationEngineOutgoingMessageData {
 }
 
 interface IntergrationEngineOutgoingMessage {
-  resource: string
-  operation: string
+  id: string
+  version: string
+  type: string
   data: IntergrationEngineOutgoingMessageData
 }
 
@@ -15,11 +19,21 @@ interface MessageAndPattern {
   message: IntergrationEngineOutgoingMessage
 }
 
-export default function (
-  providerId: string,
-  message: IntergrationEngineOutgoingMessage
-): MessageAndPattern {
-  const messagePattern = `${providerId}.${message.resource}.${message.operation}`
+interface Parameters {
+  resource: string
+  operation: string
+  data: IntergrationEngineOutgoingMessageData
+}
+
+export default function (providerId: string, params: Parameters): MessageAndPattern {
+  const messagePattern = `${providerId}.${params.resource}.${params.operation}`
+
+  const message: IntergrationEngineOutgoingMessage = {
+    id: uuidv4(),
+    type: messagePattern,
+    version: IE_MESSAGE_VERSION,
+    data: params.data
+  }
 
   return { messagePattern, message }
 }
