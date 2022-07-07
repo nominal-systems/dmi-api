cf0ba0c4e4eb04f0b9f8131d33626882b790cffe
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2022-07-07 13:49:47 +0200

    Increased wait-for timeout to 60s

--- scripts/wait-for-all.sh
+++ scripts/wait-for-all.sh
@@ -1,11 +1,11 @@
 # Waited for services
 wget -q https://github.com/eficode/wait-for/releases/latest/download/wait-for
 chmod +x ./wait-for
-./wait-for -q mysql:3306 -- echo 'MySQL is up' &
+./wait-for -q mysql:3306 -t 60 -- echo 'MySQL is up' &
 PID_MYSQL=$!
-./wait-for -q mongo:27017 -- echo 'Mongo is up' &
+./wait-for -q mongo:27017 -t 60 -- echo 'Mongo is up' &
 PID_MONGO=$!
-./wait-for -q activemq:1883 -- echo 'ActiveMQ is up' &
+./wait-for -q activemq:1883 -t 60 -- echo 'ActiveMQ is up' &
 PID_ACTIVEMQ=$!
 
 echo 'Waiting for all services to start...'
