d25c0a960e5c6ce6edc65c09cf68276b1b95a192
monsieurbelbo <gonzalo.bellver@gmail.com>
2022-08-17 15:53:31 +0200

    Notify Azure Events Hub

--- src/events/services/event-subscription.service.ts
+++ src/events/services/event-subscription.service.ts
@@ -4,6 +4,8 @@ import { EventSubscription } from '../entities/event-subscription.entity'
 import { Repository } from 'typeorm'
 import { CreateEventSubscriptionDto } from '../dto/create-event-subscription.dto'
 import { Event } from '../entities/event.entity'
+import { EventHubProducerClient } from '@azure/event-hubs'
+import { AzureNamedKeyCredential } from '@azure/core-auth'
 
 @Injectable()
 export class EventSubscriptionService {
@@ -37,6 +39,13 @@ export class EventSubscriptionService {
     })
 
     for (const subscription of subscriptions) {
+      const opts = subscription.subscription_options
+      const credential = new AzureNamedKeyCredential(opts.sa_key_name, opts.sa_key_value)
+      const producer = new EventHubProducerClient(`${opts.hub_namespace}.servicebus.windows.net`, opts.hub_name, credential)
+      const eventDataBatch = await producer.createBatch()
+      eventDataBatch.tryAdd({ body: event })
+      await producer.sendBatch(eventDataBatch)
+      await producer.close()
       this.logger.debug(`Notifying subscription: ${subscription.id} of event '${event.type}'`)
     }
   }
