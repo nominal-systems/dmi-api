6f6cf753cf35beb0a55485cad86e7b875d6d9112
monsieurbelbo <gonzalo.bellver@gmail.com>
2022-10-10 10:22:55 -0300

    OrdersService test

---
+++ src/orders/orders.service.spec.ts
@@ -0,0 +1,68 @@
+import { OrdersService } from './orders.service'
+import { Test, TestingModule } from '@nestjs/testing'
+import { ConfigService } from '@nestjs/config'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Order } from './entities/order.entity'
+import { MockUtils } from '../common/test/mock-utils'
+import { Repository } from 'typeorm'
+import { ReportsService } from '../reports/reports.service'
+import { IntegrationsService } from '../integrations/integrations.service'
+import { EventsService } from '../events/services/events.service'
+
+const repositoryMockFactory: () => MockUtils<Repository<any>> = jest.fn(() => ({}))
+
+describe('OrdersService', () => {
+  let ordersService: OrdersService
+  let configServiceMock = {
+    get: jest.fn()
+  }
+  let ordersRepositoryMock: MockUtils<Repository<Order>>
+  let testsRepositoryMock: MockUtils<Repository<Test>>
+  let reportsServiceMock = {}
+  let integrationsServiceMock = {}
+  let eventsServiceMock = {}
+  let clientMock = {}
+
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        OrdersService,
+        {
+          provide: ConfigService,
+          useValue: configServiceMock
+        },
+        {
+          provide: getRepositoryToken(Order),
+          useFactory: repositoryMockFactory
+        },
+        {
+          provide: getRepositoryToken(Test),
+          useFactory: repositoryMockFactory
+        },
+        {
+          provide: ReportsService,
+          useValue: reportsServiceMock
+        },
+        {
+          provide: IntegrationsService,
+          useValue: integrationsServiceMock
+        },
+        {
+          provide: EventsService,
+          useValue: eventsServiceMock
+        },
+        {
+          provide: 'ACTIVEMQ',
+          useValue: clientMock
+        }
+      ]
+    }).compile()
+
+    ordersService = module.get<OrdersService>(OrdersService)
+  })
+
+  it('should be defined', () => {
+    expect(ordersService).toBeDefined()
+  })
+})
