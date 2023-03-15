6e60dd37c61a4728735dc45e13ddf136d39ffd14
monsieurbelbo <gonzalo.bellver@gmail.com>
2023-03-15 18:41:04 -0300

    Fixed tests

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { HealthController } from './health.controller'
+import {
+  HealthCheckService,
+  MicroserviceHealthIndicator,
+  MongooseHealthIndicator,
+  TypeOrmHealthIndicator
+} from '@nestjs/terminus'
 
 describe('HealthController', () => {
   let controller: HealthController
 
+  const healthCheckServiceMock = {}
+  const typeOrmHealthIndicatorMock = {}
+  const mongooseHealthIndicatorMock = {}
+  const microserviceHealthIndicatorMock = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [HealthController]
+      controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: healthCheckServiceMock
+        },
+        {
+          provide: TypeOrmHealthIndicator,
+          useValue: typeOrmHealthIndicatorMock
+        },
+        {
+          provide: MongooseHealthIndicator,
+          useValue: mongooseHealthIndicatorMock
+        },
+        {
+          provide: MicroserviceHealthIndicator,
+          useValue: microserviceHealthIndicatorMock
+        }
+      ]
     }).compile()
 
     controller = module.get<HealthController>(HealthController)
