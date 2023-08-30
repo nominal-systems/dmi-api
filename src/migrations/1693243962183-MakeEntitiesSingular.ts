56c3e6f653665820f89bdbd1ca6a877463e6d615
monsieurbelbo <gonzalo.bellver@gmail.com>
2023-08-30 10:44:26 +0200

    Migration scripts fix

--- src/migrations/1693243962183-MakeEntitiesSingular.ts
+++ src/migrations/1693243962183-MakeEntitiesSingular.ts
@@ -1,20 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class MakeEntitiesSingular1693243962183 implements MigrationInterface {
-    name = 'MakeEntitiesSingular1693243962183'
+  name = 'MakeEntitiesSingular1693243962183'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`providers\` TO \`dmi\`.\`provider\` `);
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`refs\` TO \`dmi\`.\`ref\` `);
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`refs_map\` TO \`dmi\`.\`ref_map\` `);
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`provider_refs\` TO \`dmi\`.\`provider_ref\` `);
-    }
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`RENAME TABLE \`providers\` TO \`provider\` `)
+    await queryRunner.query(`RENAME TABLE \`refs\` TO \`ref\` `)
+    await queryRunner.query(`RENAME TABLE \`refs_map\` TO \`ref_map\` `)
+    await queryRunner.query(`RENAME TABLE \`provider_refs\` TO \`provider_ref\` `)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`provider_ref\` TO \`dmi\`.\`provider_refs\` `);
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`ref_map\` TO \`dmi\`.\`refs_map\` `);
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`ref\` TO \`dmi\`.\`refs\` `);
-        await queryRunner.query(`RENAME TABLE \`dmi\`.\`provider\` TO \`dmi\`.\`providers\` `);
-    }
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`RENAME TABLE \`provider_ref\` TO \`provider_refs\` `)
+    await queryRunner.query(`RENAME TABLE \`ref_map\` TO \`refs_map\` `)
+    await queryRunner.query(`RENAME TABLE \`ref\` TO \`refs\` `)
+    await queryRunner.query(`RENAME TABLE \`provider\` TO \`providers\` `)
+  }
 
 }
