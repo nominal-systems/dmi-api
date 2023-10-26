0abfc950ec9a8f5c5e55a6b040cb28b079007bd6
Juan Ignacio Viglianco <jiviglianco@gmail.com>
2023-10-26 16:28:27 -0300

    Retrieve patient when a report is updated

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -264,6 +264,7 @@ export class ReportsService {
       .leftJoinAndSelect('order.client', 'client')
       .leftJoinAndSelect('patient.identifier', 'identifier')
       .leftJoinAndSelect('report.testResultsSet', 'testResult')
+      .leftJoinAndSelect('report.patient', 'Patient')
       .leftJoinAndSelect('testResult.observations', 'observation')
       .where('order.externalId IN (:...externalOrderIds)', { externalOrderIds })
       .orderBy('testResult.seq', 'ASC')
