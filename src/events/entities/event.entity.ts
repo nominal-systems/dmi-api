7550b66fff5c0567c52ebe92a59ef8922d24d8bc
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-01-24 15:23:44 -0300

    Ensuring index for Events

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -6,7 +6,7 @@ import { EventData } from '../interfaces/event-data.interface'
 
 @Schema({ timestamps: { updatedAt: false } })
 export class Event {
-  @Prop({ unique: true })
+  @Prop({ unique: true, index: true })
   seq: number
 
   @Prop({ enum: EventNamespace })
