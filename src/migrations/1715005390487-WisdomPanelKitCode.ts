3e3d16f86fc0e129f110205c34d41f9d265f1f77
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-05-16 11:20:29 +0200

    Accepting KitCode instead of kitCode for Wisdom Panel requisition info

--- src/migrations/1715005390487-WisdomPanelKitCode.ts
+++ src/migrations/1715005390487-WisdomPanelKitCode.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class WisdomPanelKitCode1715005390487 implements MigrationInterface {
 
   public async up (queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`INSERT INTO \`provider_lab_requisition_parameter\` (\`id\`, \`name\`, \`type\`, \`required\`, \`providerId\`) VALUES (DEFAULT, ?, ?, ?, ?)`, ['kitCode', 'string', true, 'wisdom-panel'])
+    await queryRunner.query(`INSERT INTO \`provider_lab_requisition_parameter\` (\`id\`, \`name\`, \`type\`, \`required\`, \`providerId\`) VALUES (DEFAULT, ?, ?, ?, ?)`, ['KitCode', 'string', true, 'wisdom-panel'])
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DELETE FROM \`provider_lab_requisition_parameter\` WHERE \`name\` = ? AND \`providerId\` = ?`, ['kitCode', 'wisdom-panel'])
+    await queryRunner.query(`DELETE FROM \`provider_lab_requisition_parameter\` WHERE \`name\` = ? AND \`providerId\` = ?`, ['KitCode', 'wisdom-panel'])
   }
 }
