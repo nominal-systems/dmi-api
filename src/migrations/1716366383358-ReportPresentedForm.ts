11010fcef53fa36b6e4dc4b1060258851f160637
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-06-10 13:05:20 +0200

    Fixed migration

--- src/migrations/1716366383358-ReportPresentedForm.ts
+++ src/migrations/1716366383358-ReportPresentedForm.ts
@@ -4,13 +4,13 @@ export class ReportPresentedForm1716366383358 implements MigrationInterface {
   name = 'ReportPresentedForm1716366383358'
 
   public async up (queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`dmi\`.\`attachment\` ADD \`reportId\` char(36) NULL`)
-    await queryRunner.query(`ALTER TABLE \`dmi\`.\`attachment\` ADD CONSTRAINT \`FK_daebbc8b4bd00b3d39cd5894795\` FOREIGN KEY (\`reportId\`) REFERENCES \`dmi\`.\`report\` (\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`)
+    await queryRunner.query(`ALTER TABLE \`attachment\` ADD \`reportId\` char(36) NULL`)
+    await queryRunner.query(`ALTER TABLE \`attachment\` ADD CONSTRAINT \`FK_daebbc8b4bd00b3d39cd5894795\` FOREIGN KEY (\`reportId\`) REFERENCES \`report\` (\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`)
   }
 
   public async down (queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`dmi\`.\`attachment\` DROP FOREIGN KEY \`FK_daebbc8b4bd00b3d39cd5894795\``)
-    await queryRunner.query(`ALTER TABLE \`dmi\`.\`attachment\` DROP COLUMN \`reportId\``)
+    await queryRunner.query(`ALTER TABLE \`attachment\` DROP FOREIGN KEY \`FK_daebbc8b4bd00b3d39cd5894795\``)
+    await queryRunner.query(`ALTER TABLE \`attachment\` DROP COLUMN \`reportId\``)
   }
 
 }
