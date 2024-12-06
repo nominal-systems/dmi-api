fcc2536306b80c9511cfd0bc38995493c7059c81
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-12-06 16:16:33 +0100

    Fixed migration script

--- src/migrations/1733490175397-ClientContact.ts
+++ src/migrations/1733490175397-ClientContact.ts
@@ -4,13 +4,13 @@ export class ClientContact1733490175397 implements MigrationInterface {
     name = 'ClientContact1733490175397'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`dmi\`.\`client\` ADD \`contactPhone\` varchar(255) NULL`);
-        await queryRunner.query(`ALTER TABLE \`dmi\`.\`client\` ADD \`contactEmail\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`client\` ADD \`contactPhone\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`client\` ADD \`contactEmail\` varchar(255) NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`dmi\`.\`client\` DROP COLUMN \`contactEmail\``);
-        await queryRunner.query(`ALTER TABLE \`dmi\`.\`client\` DROP COLUMN \`contactPhone\``);
+        await queryRunner.query(`ALTER TABLE \`client\` DROP COLUMN \`contactEmail\``);
+        await queryRunner.query(`ALTER TABLE \`client\` DROP COLUMN \`contactPhone\``);
     }
 
 }
