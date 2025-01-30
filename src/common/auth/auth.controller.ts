eedae8beee0aa56e010baa4c61b95057b110b625
monsieurbelbo <gonzalo.bellver@gmail.com>
2025-01-30 16:47:15 +0100

    Redirect to Okta SSO page

--- src/common/auth/auth.controller.ts
+++ src/common/auth/auth.controller.ts
@@ -1,25 +1,46 @@
 import { Controller, Get, Req, Res } from '@nestjs/common'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { ConfigService } from '@nestjs/config'
 import fastifyPassport from 'fastify-passport'
 
 @Controller('auth')
 export class AuthController {
+  constructor (private readonly configService: ConfigService) {
+  }
+
   @Get('login')
-  async login (@Req() req: FastifyRequest, @Res() res: FastifyReply) {
-    await fastifyPassport.authenticate('oidc')(req, res)
+  async login (@Req() req: FastifyRequest, @Res() res: FastifyReply): Promise<void> {
+    console.log('login()') // TODO(gb): remove trace
+    const authenticate = fastifyPassport.authenticate('oidc', {
+      successRedirect: '/ui/admin',
+      failureRedirect: '/ui/login',
+      authInfo: false
+    }) as (req: FastifyRequest, res: FastifyReply) => Promise<void>
+
+    await authenticate(req, res)
   }
 
   @Get('callback')
-  async callback (@Req() req: FastifyRequest, @Res() res: FastifyReply) {
-    await fastifyPassport.authenticate('oidc', {
-      successRedirect: '/protected',
-      failureRedirect: '/auth/login'
-    })(req, res)
+  async callback (@Req() req: FastifyRequest, @Res() res: FastifyReply): Promise<void> {
+    console.log('callback') // TODO(gb): remove trace
+    const authenticate = fastifyPassport.authenticate('oidc', {
+      successRedirect: '/ui/admin',
+      failureRedirect: '/ui/login',
+      authInfo: false
+    }) as (req: FastifyRequest, res: FastifyReply) => Promise<void>
+
+    await authenticate(req, res)
   }
 
   @Get('logout')
-  async logout (@Req() req: FastifyRequest, @Res() res: FastifyReply) {
-    req.logout()
-    res.redirect('/')
+  async logout (@Req() req: FastifyRequest, @Res() res: FastifyReply): Promise<void> {
+    await req.logout()
+
+    const oktaDomain = this.configService.get<string>('okta.domain')
+    const clientId = this.configService.get<string>('okta.clientId')
+    const baseUrl = this.configService.get<string>('app.baseUrl', 'http://localhost:3000')
+
+    const logoutUrl = `https://${oktaDomain}/oauth2/default/v1/logout?client_id=${clientId}&post_logout_redirect_uri=${baseUrl}`
+    return res.redirect(logoutUrl)
   }
 }
