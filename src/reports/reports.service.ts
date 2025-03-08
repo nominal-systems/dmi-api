5a169f9c45e497cbbc67502a9e4e0b746c88b39f
Luis Greco <lgreco@dc.uba.ar>
2025-03-08 01:06:53 -0300

    Modify updateReportResults to ensure the order of result items is preserved.

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -327,7 +327,7 @@ export class ReportsService {
         this.logger.warn(`Multiple test result item status for test result '${providerTestResult.code}'`)
       }
 
-      const existingTestResult = report.testResultsSet.find(testResult => testResult.code === providerTestResult.code)
+      const existingTestResult = report.testResultsSet.find(testResult => testResult.code === providerTestResult.code && testResult.seq === providerTestResult.seq)
 
       if (existingTestResult != null) {
         existingTestResult.seq = providerTestResult.seq
