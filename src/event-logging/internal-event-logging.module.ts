8f339cc6f3c970e09497b96c02c3443bec3720e0
monsieurbelbo <gonzalo.bellver@gmail.com>
2025-04-30 13:44:23 +0200

    [#222] Extracting accession IDs from orders' requisitionId

--- src/event-logging/internal-event-logging.module.ts
+++ src/event-logging/internal-event-logging.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { InternalEventLoggingService } from './internal-event-logging.service'
 import { InternalEventLoggingInterceptor } from './internal-event-logging.interceptor'
 import { InternalEvent, InternalEventSchema } from './schemas/internal-event.schema'
+import { OrdersModule } from '../orders/orders.module'
 
 @Module({
   imports: [
+    forwardRef(() => OrdersModule),
     MongooseModule.forFeature([
       { name: InternalEvent.name, schema: InternalEventSchema }
     ])
