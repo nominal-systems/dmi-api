a1e4c35a8eb0cad8d187439aa5ca20bf5f158c46
monsieurbelbo <gonzalo.bellver@gmail.com>
2025-05-01 11:20:08 +0200

    Fix typo

--- src/internal-event-logging/schemas/internal-event.schema.ts
+++ src/internal-event-logging/schemas/internal-event.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Document } from 'mongoose'
 
-@Schema({ collection: 'iternal_events', timestamps: true })
+@Schema({ collection: 'internal_events', timestamps: true })
 export class InternalEvent {
   @Prop({ index: true })
   createdAt: Date
