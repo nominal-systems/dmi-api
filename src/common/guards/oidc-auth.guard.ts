9816dc92a50a82abb36df2136ade62c5bdc9ce09
monsieurbelbo <gonzalo.bellver@gmail.com>
2025-05-21 09:57:58 +0200

    Added Base URL to auth redirect URL.

--- src/common/guards/oidc-auth.guard.ts
+++ src/common/guards/oidc-auth.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable, Logger } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { FastifyReply } from 'fastify'
+import { ConfigService } from '@nestjs/config'
 
 // Define the extended FastifyReply type
 interface ExtendedFastifyReply extends FastifyReply {
@@ -12,9 +13,14 @@ interface ExtendedFastifyReply extends FastifyReply {
 export class OidcAuthGuard extends AuthGuard('oidc') {
   private readonly logger = new Logger(OidcAuthGuard.name)
 
+  constructor (private readonly configService: ConfigService) {
+    super()
+  }
+
   async canActivate (context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest()
     const response = context.switchToHttp().getResponse<ExtendedFastifyReply>()
+    const baseUrl = this.configService.get<string>('baseUrl', '')
 
     // Check if we're already in the auth flow
     const isAuthPath: boolean = request.url.startsWith('/auth/')
@@ -29,7 +35,7 @@ export class OidcAuthGuard extends AuthGuard('oidc') {
 
     if (!request.isAuthenticated?.()) {
       this.logger.debug('User not authenticated, redirecting to /auth/login')
-      response.status(302).redirect('/auth/login')
+      response.status(302).redirect(`${baseUrl}/auth/login`)
       return false
     }
 
@@ -40,7 +46,7 @@ export class OidcAuthGuard extends AuthGuard('oidc') {
     } catch (error) {
       this.logger.error(`Authentication error: ${error.message}`)
       this.logger.error(error.stack)
-      response.status(302).redirect('/auth/login')
+      response.status(302).redirect(`${baseUrl}/auth/login`)
       return false
     }
   }
