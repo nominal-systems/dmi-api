e167414f678971134d68d4b0179ed1be4af72cee
Luis Greco <lgreco@dc.uba.ar>
2026-03-05 17:26:45 -0300

    Fix auth redirect paths to include baseUrl prefix

--- src/common/filters/frontend-http-exception.filter.ts
+++ src/common/filters/frontend-http-exception.filter.ts
@@ -1,8 +1,15 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { FastifyReply } from 'fastify'
 
 @Catch(HttpException)
 export class FrontendHttpExceptionFilter implements ExceptionFilter {
+  private readonly baseUrl: string
+
+  constructor (private readonly configService: ConfigService) {
+    this.baseUrl = this.configService.get<string>('baseUrl', '') ?? ''
+  }
+
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   catch (exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
@@ -11,7 +18,7 @@ export class FrontendHttpExceptionFilter implements ExceptionFilter {
 
     if (status === 401) {
       // eslint-disable-next-line no-void
-      void response.redirect('/ui/login')
+      void response.redirect(`${this.baseUrl}/ui/login`)
     } else {
       // If the status is not 401, send the original error response.
       // eslint-disable-next-line no-void
